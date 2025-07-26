84b565ee8bfae50024ee8c1deea25cc6891a3087
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-07-26 19:04:38 -0400

    add size and type request parameters

--- src/main/java/com/example/qrcodeservice/controller/QRCodeController.java
+++ src/main/java/com/example/qrcodeservice/controller/QRCodeController.java
@@ -7,9 +7,11 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.awt.image.BufferedImage;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -21,7 +23,20 @@ public class QRCodeController {
     }
 
     @GetMapping("/qrcode")
-    public ResponseEntity<BufferedImage> getImage() {
+    public ResponseEntity<?> getImage(
+            @RequestParam Integer size,
+            @RequestParam String type
+    ) {
+        if (size < 150 || size > 350) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of("error", "Image size must be between 150 and 350 pixels"));
+        } else if (!(type.equals("png") || type.equals("jpeg") || type.equals("gif"))) {
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(Map.of("error", "Only png, jpeg and gif image types are supported"));
+        }
+
         BufferedImage bufferedImage = QRCodeService.generateImage(250, 250);
 
         return ResponseEntity
