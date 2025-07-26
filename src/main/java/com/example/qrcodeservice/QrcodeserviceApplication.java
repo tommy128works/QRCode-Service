b03f45e219624d05e95fdadbd81be41268d6abab
Tommy <57922367+tommy128works@users.noreply.github.com>
2025-07-26 14:07:56 -0400

    spring initializr with Spring Web

---
+++ src/main/java/com/example/qrcodeservice/QrcodeserviceApplication.java
@@ -0,0 +1,13 @@
+package com.example.qrcodeservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QrcodeserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QrcodeserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/qrcodeservice/QrcodeserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.qrcodeservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QrcodeserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
